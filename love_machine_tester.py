d43911c851708937806d375f4d753389a2228b14
benjamin-olivera <benjamin.oliveracolonio@gmail.com>
2024-04-23 18:57:39 -0500

    adding an alternative short way

--- love_machine_tester.py
+++ love_machine_tester.py
@@ -40,6 +40,32 @@ elif int(concatenated) >=40 and int(concatenated) >= 50:
   print(f"Your score is {concatenated}, you are alright together.")
 else:
   print(f"Your score is {concatenated}")
+
+
+#the short way:
+'''
+combined_names = name1 + name2
+names_lower = combined_names.lower()
+t = names_lower.count("t")
+r = names_lower.count("r")
+u = names_lower.count("u")
+e = names_lower.count("e")
+first_numb = t + r +u +e 
+l = names_lower.count("l")
+o = names_lower.count("o")
+v = names_lower.count("v")
+e = names_lower.count("e")
+second_numb = l + o + v + e
+love_score = int(str(first_numb) + str(second_numb))
+
+if love_score < 10 or love_score > 90:
+  print(f"Your score is {love_score}, you go together like coke and mentos.")
+elif love_score >= 40 and love_score <=50:
+  print(f"Your score is {love_score}, you are alright together.")
+else:
+  print(f"Your score is {love_score}.")
+'''
+
   
 
     
